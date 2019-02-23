b139376447f5ba5ead9603c8fb12088dd7d2176b
stormlin <lin-jinting@qq.com>
2019-02-23 21:49:58 +0800

    Add help message for main program

--- src/main/java/com/stormlin/Program.java
+++ src/main/java/com/stormlin/Program.java
@@ -5,7 +5,16 @@ import com.stormlin.command.*;
 public class Program {
 
     private static void usage() {
-        System.out.println("Usage");
+        System.out.println("usage: todo <command> [arguments]");
+        System.out.println("");
+        System.out.println("All supported commands are:");
+        System.out.println("  init    Initialize an empty list in folder ./todo");
+        System.out.println("  status  Print all tasks in current list");
+        System.out.println("  add     Add a new task to current list");
+        System.out.println("  remove  Remove a task from current list");
+        System.out.println("  branch  Add, remove or switch list branch");
+        System.out.println("");
+        System.out.println("Use \"todo <command> -h\" or \"todo <command> --help\" to get help for specific command");
     }
 
     public static void main(String[] args) {
@@ -35,6 +44,12 @@ public class Program {
         case "branch":
             CommandBranch branch = new CommandBranch();
             branch.execute(args);
+            break;
+        case "-h":
+            Program.usage();
+            break;
+        case "--help":
+            Program.usage();
             break;
         default:
             System.out.println("Wrong command.");
